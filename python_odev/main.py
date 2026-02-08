39955d8aa7675f9296206549e05ede39d1ca88f6
aristotakla <isoozzturk@hotmail.com>
2026-02-08 18:32:28 +0300

    Prohe yapısı düzeltildi ve ilk kodlar tekrar eklendi

---
+++ python_odev/main.py
@@ -0,0 +1,6 @@
+# 0'dan 4'e kadar olan sayıları ekrana yazdıran basit bir döngü
+print("Sayilar listeleniyor:")
+for i in range(5):
+    print("Su an ki sayi:", i)
+
+print("Bu ikinci commit icin eklenen satirdir.")
